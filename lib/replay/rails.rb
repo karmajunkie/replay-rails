b1053aee6664e55a13711f09a570bf6d6072a2ed
karmajunkie <keith.gaddis@gmail.com>
2014-03-27 11:02:55 -0500

    initial commit?

---
+++ lib/replay/rails.rb
@@ -0,0 +1,7 @@
+require "replay/rails/version"
+
+module Replay
+  module Rails
+    # Your code goes here...
+  end
+end
---
+++ lib/replay/rails/version.rb
@@ -0,0 +1,5 @@
+module Replay
+  module Rails
+    VERSION = "0.0.1"
+  end
+end
