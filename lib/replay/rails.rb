8f26c1eca19c049b57dd12ecb180960e6d8839a7
karmajunkie <keith.gaddis@gmail.com>
2014-03-27 11:04:18 -0500

    More initial stuff

--- lib/replay/rails.rb
+++ lib/replay/rails.rb
@@ -1,7 +1,54 @@
+require 'replay'
 require "replay/rails/version"
 
 module Replay
   module Rails
-    # Your code goes here...
+    class ActiveRecordEventStore 
+      class Event < ActiveRecord::Base
+        self.table_name = "domain_events"
+
+        def readonly?
+          true
+        end
+
+        def destroy
+          raise ActiveRecord::ReadOnlyRecord.new
+        end
+        def delete
+          raise ActiveRecord::ReadOnlyRecord.new
+        end
+
+        def self.delete_all(*args)
+          raise ActiveRecord::ReadOnlyRecord.new
+        end
+
+        def self.delete(*args)
+          raise ActiveRecord::ReadOnlyRecord.new
+        end
+
+        def event
+          event_type.constantize.new(JSON.parse(event_data))
+        end
+
+        def self.events_for(stream_id)
+          where(:stream_id => stream_id).order('created_at asc')
+        end
+
+        def self.event_stream(stream_id)
+          events_for(stream_id).to_a.map(&:event)
+        end
+
+      end
+
+      def self.published(stream_id, event)
+        Event.create(stream_id: stream_id, event_type: event.class.to_s, event_data: event.to_json)
+      end
+
+      def self.event_stream(stream_id)
+        Event.event_stream(stream_id)
+      end
+    end
   end
 end
+
+Replay::Backends.register(:active_record, Replay::Rails::ActiveRecordEventStore)
--- replay-rails.gemspec
+++ replay-rails.gemspec
@@ -8,9 +8,9 @@ Gem::Specification.new do |spec|
   spec.version       = Replay::Rails::VERSION
   spec.authors       = ["karmajunkie"]
   spec.email         = ["keith.gaddis@gmail.com"]
-  spec.description   = %q{TODO: Write a gem description}
-  spec.summary       = %q{TODO: Write a gem summary}
-  spec.homepage      = ""
+  spec.description   = %q{Includes some Rails-support code for Replay}
+  spec.summary       = %q{replay-rails provides some glue and support code for using Replay with Rails}
+  #spec.homepage      = "http://"
   spec.license       = "MIT"
 
   spec.files         = `git ls-files`.split($/)
@@ -20,4 +20,6 @@ Gem::Specification.new do |spec|
 
   spec.add_development_dependency "bundler", "~> 1.3"
   spec.add_development_dependency "rake"
+  spec.add_runtime_dependency "replay", "~>0.1.0"
+  spec.add_runtime_dependency "rails", ">3.2.0"
 end
